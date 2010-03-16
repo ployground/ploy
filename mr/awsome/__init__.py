35f4b7a28917f7107b7b8afff5189adcb9a91a84
Florian Schulze <florian.schulze@gmx.net>
2010-03-16 12:27:33 +0100

    Move Template class into it's own file.

--- mr/awsome/__init__.py
+++ mr/awsome/__init__.py
@@ -1,11 +1,11 @@
 from boto.ec2.securitygroup import GroupOrCIDR
 from boto.exception import EC2ResponseError
 from mr.awsome.config import Config
+from mr.awsome.template import Template
 from StringIO import StringIO
 from textwrap import dedent
 import boto.ec2
 import datetime
-import email
 import fabric.main
 import fabric.network
 import fabric.state
@@ -88,46 +88,6 @@ class Securitygroups(object):
                 else:
                     sg.authorize(*connection)
         return sg
-
-
-class Template(object):
-    def __init__(self, path):
-        self.path = path
-        self.template = email.message_from_file(open(path))
-
-    def __call__(self, **kwargs):
-        options = {}
-        for key, value in self.template.items():
-            commands, value = value.rsplit(None, 1)
-            for cmd in commands.split(','):
-                if cmd == 'file':
-                    path = value
-                    if not os.path.isabs(path):
-                        path = os.path.join(os.path.dirname(self.path), path)
-                    value = open(path).read()
-                elif cmd == 'base64':
-                    value = value.encode("base64")
-                elif cmd == 'format':
-                    value = value.format(**kwargs)
-                elif cmd == 'template':
-                    path = value
-                    if not os.path.isabs(path):
-                        path = os.path.join(os.path.dirname(self.path), path)
-                    value = Template(path)(**kwargs)
-                elif cmd == 'gzip':
-                    s = StringIO()
-                    gz = gzip.GzipFile(mode='wb', fileobj=s)
-                    gz.write(value)
-                    gz.close()
-                    value = s.getvalue()
-                elif cmd == 'escape_eol':
-                    value = value.replace('\n', '\\n')
-                else:
-                    raise ValueError("Unknown command '%s' for option '%s' in startup script '%s'." % (cmd, key, self.path))
-            options[key] = value
-        for key in kwargs:
-            options[key] = kwargs[key]
-        return self.template.get_payload().format(**options)
 
 
 class Server(object):
