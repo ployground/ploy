75b77de58d1006ad5be7eedae4c83e618d38374f
Florian Schulze <florian.schulze@gmx.net>
2011-05-04 14:22:39 +0200

    Add startup_script tests.

---
+++ mr/awsome/tests/test_common.py
@@ -0,0 +1,135 @@
+from StringIO import StringIO
+from mock import patch
+from mr.awsome.common import StartupScriptMixin
+from mr.awsome.config import Config, StartupScriptMassager
+from unittest2 import TestCase
+import os
+import shutil
+import tempfile
+
+
+class MockMaster(object):
+    def __init__(self, main_config):
+        self.main_config = main_config
+
+
+class MockInstance(StartupScriptMixin):
+    sectiongroupname = "instance"
+
+    def __init__(self):
+        self.id = "foo"
+
+
+class StartupScriptTests(TestCase):
+    def setUp(self):
+        self.directory = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.directory)
+        del self.directory
+
+    def _create_config(self, contents, path=None):
+        contents = StringIO(contents)
+        config = Config(contents, path=path)
+        config._add_massager(
+            StartupScriptMassager('instance', 'startup_script'))
+        return config.parse()
+
+    def testNoStartupScript(self):
+        instance = MockInstance()
+        config = self._create_config("[instance:foo]")
+        instance.master = MockMaster(config)
+        result = instance.startup_script()
+        self.assertMultiLineEqual(result, "")
+
+    def testMissingStartupScript(self):
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        with self.assertRaises(IOError) as exc:
+            instance.startup_script()
+        self.assertEqual('No such file or directory', exc.exception[1])
+        self.assertEqual(os.path.join(self.directory, 'foo'), exc.exception[2])
+
+    def testEmptyStartupScript(self):
+        with open(os.path.join(self.directory, 'foo'), 'w') as f:
+            f.write("")
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        result = instance.startup_script()
+        self.assertMultiLineEqual(result, "")
+
+    def testGzip(self):
+        with open(os.path.join(self.directory, 'foo'), 'w') as f:
+            f.write("")
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = gzip:foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        result = instance.startup_script()
+        self.assertMultiLineEqual(result[:52], "\n".join([
+            "#!/bin/bash",
+            "tail -n+4 $0 | gunzip -c | bash",
+            "exit $?",
+            ""]))
+        payload = result[52:]
+        header = payload[:10]
+        body = payload[10:]
+        self.assertEqual(header[:4], "\x1f\x8b\x08\x00") # magic + compression + flags
+        self.assertEqual(header[8:], "\x02\xff") # extra flags + os
+        self.assertEqual(body, "\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+
+    def testMaxSizeOk(self):
+        with open(os.path.join(self.directory, 'foo'), 'w') as f:
+            f.write("")
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        instance.max_startup_script_size = 10
+        result = instance.startup_script()
+        self.assertMultiLineEqual(result, "")
+
+    def testMaxSizeExceeded(self):
+        with open(os.path.join(self.directory, 'foo'), 'w') as f:
+            f.write("aaaaabbbbbccccc")
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        instance.max_startup_script_size = 10
+        with self.assertRaises(SystemExit):
+            instance.startup_script()
+
+    def testMaxSizeExceededDebug(self):
+        with open(os.path.join(self.directory, 'foo'), 'w') as f:
+            f.write("aaaaabbbbbccccc")
+        instance = MockInstance()
+        config = self._create_config(
+            "\n".join([
+                "[instance:foo]",
+                "startup_script = foo"]),
+            path=self.directory)
+        instance.master = MockMaster(config)
+        instance.max_startup_script_size = 10
+        with patch('mr.awsome.common.log') as LogMock:
+            instance.startup_script(debug=True)
+            LogMock.error.assert_called_with('Startup script too big.')
