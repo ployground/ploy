c61a6da69b9a562c0a80bfcaabe24ca2f69bc7a6
Florian Schulze <florian.schulze@gmx.net>
2013-09-20 19:00:34 +0200

    Bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-version = "0.13"
+version = "0.14"
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
