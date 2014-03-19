c8275994efd9031c85e21accbb65927f97957ae0
Florian Schulze <florian.schulze@gmx.net>
2014-03-19 08:03:34 +0100

    Refactor config to massage values on get instead of load.

--- mr/awsome/config.py
+++ mr/awsome/config.py
@@ -1,5 +1,7 @@
 from mr.awsome.common import Hooks
 from ConfigParser import RawConfigParser
+from UserDict import DictMixin
+from weakref import proxy
 import os
 import warnings
 
@@ -16,8 +18,8 @@ class BaseMassager(object):
         self.sectiongroupname = sectiongroupname
         self.key = key
 
-    def __call__(self, main_config, sectionname):
-        return main_config[self.sectiongroupname][sectionname][self.key]
+    def __call__(self, config, sectionname):
+        return config._dict[self.key]
 
 
 class BooleanMassager(BaseMassager):
@@ -40,7 +42,7 @@ class PathMassager(BaseMassager):
         value = BaseMassager.__call__(self, config, sectionname)
         value = os.path.expanduser(value)
         if not os.path.isabs(value):
-            value = os.path.join(config.path, value)
+            value = os.path.join(config._config.path, value)
         return value
 
 
@@ -85,7 +87,7 @@ class StartupScriptMassager(BaseMassager):
             value = value[5:]
             result['gzip'] = True
         if not os.path.isabs(value):
-            value = os.path.join(config.path, value)
+            value = os.path.join(config._config.path, value)
         result['path'] = value
         return result
 
@@ -99,7 +101,47 @@ class UserMassager(BaseMassager):
         return value
 
 
-class Config(dict):
+class ConfigSection(DictMixin):
+    def __init__(self, *args, **kw):
+        self._dict = dict(*args, **kw)
+        self.sectionname = None
+        self.sectiongroupname = None
+        self._config = None
+
+    def __delitem__(self, key):
+        del self._dict[key]
+
+    def __getitem__(self, key):
+        if key == '__groupname__':
+            return self.sectiongroupname
+        if key == '__name__':
+            return self.sectionname
+        if key in self._dict and self._config is not None:
+            massage = self._config.massagers.get((self.sectiongroupname, key))
+            if not callable(massage):
+                massage = self._config.massagers.get((None, key))
+                if callable(massage):
+                    return massage(self, self.sectiongroupname, self.sectionname)
+            else:
+                return massage(self, self.sectionname)
+        return self._dict[key]
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+
+    def keys(self):
+        return self._dict.keys()
+
+    def copy(self):
+        new = ConfigSection()
+        new._dict = self._dict.copy()
+        new.sectionname = self.sectionname
+        new.sectiongroupname = self.sectiongroupname
+        new._config = self._config
+        return new
+
+
+class Config(ConfigSection):
     def _add_massager(self, massager):
         key = (massager.sectiongroupname, massager.key)
         if key in self.massagers:
@@ -130,6 +172,7 @@ class Config(dict):
         del section['<']
 
     def __init__(self, config, path=None, bbb_config=False, plugins=None):
+        ConfigSection.__init__(self)
         self.config = config
         self.path = path
         self.massagers = {}
@@ -149,14 +192,19 @@ class Config(dict):
         else:
             _config.read(self.config)
             self.path = os.path.dirname(self.config)
-        for section in _config.sections():
-            if ':' in section:
-                sectiongroupname, sectionname = section.split(':')
+        for configsection in _config.sections():
+            if ':' in configsection:
+                sectiongroupname, sectionname = configsection.split(':')
             else:
-                sectiongroupname, sectionname = 'global', section
-            items = dict(_config.items(section))
-            sectiongroup = self.setdefault(sectiongroupname, {})
-            sectiongroup.setdefault(sectionname, {}).update(items)
+                sectiongroupname, sectionname = 'global', configsection
+            sectiongroup = self.setdefault(sectiongroupname, ConfigSection())
+            if sectionname not in sectiongroup:
+                section = ConfigSection()
+                section.sectiongroupname = sectiongroupname
+                section.sectionname = sectionname
+                section._config = proxy(self)
+                sectiongroup[sectionname] = section
+            sectiongroup[sectionname].update(_config.items(configsection))
         if 'plugin' in self:
             warnings.warn("The 'plugin' section isn't used anymore.")
             del self['plugin']
@@ -173,26 +221,10 @@ class Config(dict):
                         'massagers')(self, sectionname)
                     for massager in massagers:
                         self._add_massager(massager)
-                for key in section:
-                    massage = self.massagers.get((sectiongroupname, key))
-                    if not callable(massage):
-                        massage = self.massagers.get((None, key))
-                        if callable(massage):
-                            section[key] = massage(self, sectiongroupname, sectionname)
-                    else:
-                        section[key] = massage(self, sectionname)
         return self
 
     def get_section_with_overrides(self, sectiongroupname, sectionname, overrides):
         config = self[sectiongroupname][sectionname].copy()
-        config['__groupname__'] = sectiongroupname
-        config['__name__'] = sectionname
-        if overrides is None:
-            return config
-        dummy = {sectiongroupname: {sectionname: config}}
-        for key in overrides:
-            config[key] = overrides[key]
-            massage = self.massagers.get((sectiongroupname, key))
-            if callable(massage):
-                config[key] = massage(dummy, sectionname)
+        if overrides is not None:
+            config._dict.update(overrides)
         return config
