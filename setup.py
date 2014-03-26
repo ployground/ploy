454bdafe4f5b58882dcdbcba2e6d221e9a7d1317
Florian Schulze <florian.schulze@gmx.net>
2014-03-26 11:23:42 +0100

    Moved setuptools-git from setup.py to tox.ini, it's only needed for releases and testing.

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
     packages=['mr'],
     namespace_packages=['mr'],
     install_requires=install_requires,
-    setup_requires=[
-        'setuptools-git'],
     entry_points="""
         [console_scripts]
         aws = mr.awsome:aws
