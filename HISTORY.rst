b92498be4cafb820814cea63ee1a40a7c308c97f
Florian Schulze <mail@florian-schulze.net>
2022-08-17 13:05:21 +0200

    Prepare ploy 2.0.0.

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,8 +1,8 @@
 Changelog
 =========
 
-2.0.0b6 - Unreleased
---------------------
+2.0.0 - 2022-08-17
+------------------
 
 * Dropped support for Python 3.6.
   [fschulze]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-version = "2.0.0b6"
+version = "2.0.0"
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
